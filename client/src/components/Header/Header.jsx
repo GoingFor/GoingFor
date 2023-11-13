64a0e3fdf37967076d9a6547877a6f6a2f276227
Melanie Silies <melanie.silies@web.de>
2023-11-13 15:21:13 +0100

    delete: regions from Navbar

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -3,7 +3,6 @@ import { NavLink } from "react-router-dom";
 import "./style.css";
 import HamburgerImage from '/src/assets/hamburger.png';
 import goingfor_logo from '../../assets/goingfor_logo.png';
-import CreateEvent from "../../pages/CreateEvent/CreateEvent";
 
 const Header = () => {
     const [showNavbar, setShowNavbar] = useState(false);
@@ -58,65 +57,6 @@ const Header = () => {
                                 <li><NavLink to="/blues">Blues</NavLink></li>
                                 <li><NavLink to="/reagae">Reagae</NavLink></li>
                                 <li><NavLink to="/hiphop">Hip-Hop</NavLink></li>
-                            </ul>
-                        )}
-                    </li>
-                    <li onClick={handleRegionsMenu}>
-                        <span>Regionen</span>
-                        {showRegionsMenu && (
-                            <ul className="submenu">
-                                <li><NavLink to="/alleregionen">
-                                    Überall</NavLink></li>
-                                <li><NavLink to="/nds">
-                                    Niedersachsen</NavLink></li>
-                                <li><NavLink to="/nrw">
-                                    Nordrhein-Westfahlen</NavLink></li>
-                                <li><NavLink to="/schleswig">
-                                    Schleswig-Holstein</NavLink></li>
-                                <li><NavLink to="/meckpomm">
-                                    Mecklenburg Vorpommern</NavLink></li>
-                                <li><NavLink to="/hamburg">
-                                    Hamburg</NavLink></li>
-                                <li><NavLink to="/bremen">
-                                    Bremen</NavLink></li>
-                                <li><NavLink to="/brandenburg">
-                                    Brandenburg</NavLink></li>
-                                <li><NavLink to="/berlin">
-                                    Berlin</NavLink></li>
-                                <li><NavLink to="/sachsen">
-                                    Sachsen</NavLink></li>
-                                <li><NavLink to="/sachsen-anhalt">
-                                    Sachsen-Anhalt</NavLink></li>
-                                <li><NavLink to="/thüringen">
-                                    Thüringen</NavLink></li>
-                                <li><NavLink to="/saarland">
-                                    Saarland</NavLink></li>
-                                <li><NavLink to="/baden">
-                                    Baden-Würtemberg</NavLink></li>
-                                <li><NavLink to="/rheinland">
-                                    Rheinland-Pfalz</NavLink></li>
-                                <li><NavLink to="/bayern">
-                                    Bayern</NavLink></li>
-                                <li><NavLink to="/hessen">
-                                    Hessen</NavLink></li>
-                                    <li><NavLink to="/nl">
-                                    Niederlande</NavLink></li>
-                                <li><NavLink to="/belgien">
-                                    Belgien</NavLink></li>
-                                <li><NavLink to="/lux">
-                                    Luxemburg</NavLink></li>
-                                <li><NavLink to="/frankreich">
-                                    Frankreich</NavLink></li>
-                                <li><NavLink to="/swiss">
-                                    Schweiz</NavLink></li>
-                                <li><NavLink to="/austria">
-                                    Österreich</NavLink></li>
-                                <li><NavLink to="/tschech">
-                                    Tschechien</NavLink></li>
-                                <li><NavLink to="/polen">
-                                    Polen</NavLink></li>
-                                <li><NavLink to="/denmark">
-                                    Dänemark</NavLink></li> 
                             </ul>
                         )}
                     </li>
