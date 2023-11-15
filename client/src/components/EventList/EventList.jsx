39d80604ed592c56f5bb76ee56a9fabf4dc91b4a
Melanie Silies <melanie.silies@web.de>
2023-11-15 18:29:35 +0100

    fix: make Events in Eventlist clickable

--- client/src/components/EventList/EventList.jsx
+++ client/src/components/EventList/EventList.jsx
@@ -41,16 +41,15 @@ const EventList = () => {
           {events.map((event) => (
             <div key={event._id} className="EventList-card">
                 {event.photos && <img className="EventList-img" src={event.photos} alt={event.name} />}
-              <h1 className="EventList-h1">{event.name}</h1>
+              <h1 className="EventList-h1">
+              <a href={`/home/event/${event._id}`}>{event.name}</a>
+              </h1>
               <h3 className="EventList-h3-date">
                 {new Date(event.startDate).toLocaleDateString()} - {new Date(event.endDate).toLocaleDateString()}
               </h3>
-              <h3 className="EventList-h3">{event.zipCode} {event.city}</h3>
+              <h3 className="EventList-h3">{event.postcode} {event.city}</h3>
               <h3 className="EventList-h3">{event.genre}</h3>
               <p className="EventList-text">{event.description}</p>
-              <Link to={`home/event/${event.id}`}>
-              <b>Weitere Infos</b>
-            </Link>
             </div>
           ))}
         </div>
